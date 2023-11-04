d34fea8c18d369e7c5d54f118f398b38a1a4dfb0
Frank Lee <flee63@gatech.edu>
2023-11-04 12:16:41 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ReactFullpage from "@fullpage/react-fullpage";
 import "./App.css";
 import Video from "./Video";
 
@@ -6,6 +7,23 @@ function App() {
     <>
       <Video></Video>
       <Video></Video>
+      <ReactFullpage
+        //fullpage options
+        licenseKey={"gplv3-license"}
+        scrollingSpeed={1000} /* Options here */
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <Video></Video>
+              </div>
+              <div className="section">
+                <Video></Video>
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
     </>
   );
 }
